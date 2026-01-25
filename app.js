a0a4dcd12897fdbcdde5fef0d9bec9273b7e341c
imadckk <motoradc@gmail.com>
2026-01-25 09:58:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const supabaseUrl = 'https://dorkygsgobhcagtqydjb.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvcmt5Z3Nnb2JoY2FndHF5ZGpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwOTc0MzcsImV4cCI6MjA3NjY3MzQzN30.bNCo8Ijj2DIr-c34P7U-lb6QK69D8OzO2sCd6SOwaW0'
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let currentDate = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Kuala_Lumpur"}));
+let currentDate = new Date();
 let currentLocationId = null;
 let locations = [];
 
